9aab96dc7f86e1f4b1130de9a1c0dabcfdeeb17e
Sora Tonami <ms0503@outlook.com>
2024-10-07 20:13:11 +0900

    [librobo-compat] ドキュメントを追加。

--- librobo-compat/src/esp32_ds4_driver.rs
+++ librobo-compat/src/esp32_ds4_driver.rs
@@ -1,7 +1,11 @@
+//! esp32_ds4_driverとの連携モジュール。
+
 use esp32_ds4_driver::Sticks;
 use robo::controller::Sticks as RoboSticks;
 
+/// [`Sticks`][RoboSticks]への変換を提供。
 pub trait ToRoboSticks {
+    /// デッドゾーン情報と自身から[`Sticks`][RoboSticks]へ変換する。
     fn to_robo_sticks(&self, dead_zone: u8) -> RoboSticks;
 }
 
--- librobo-compat/src/lib.rs
+++ librobo-compat/src/lib.rs
@@ -1,2 +1,4 @@
+//! 他ライブラリとの連携クレート。
+
 #[cfg(feature = "esp32-ds4-driver")]
 pub mod esp32_ds4_driver;
